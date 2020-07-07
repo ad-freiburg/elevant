6a0fca2379c799b0a0c4e8865c67fd64d8438d38
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2020-07-07 15:58:43 +0200

    support files with paragraphs as lines

--- link_wiki_entities.py
+++ link_wiki_entities.py
@@ -2,6 +2,7 @@ import sys
 import spacy
 
 from src.wikipedia_corpus import WikipediaCorpus
+from src.paragraph import Paragraph, paragraph_from_json
 from src.link_entity_linker import LinkEntityLinker
 from src.trained_entity_linker import TrainedEntityLinker
 from src.alias_entity_linker import AliasEntityLinker
@@ -9,10 +10,27 @@ from src.coreference_entity_linker import CoreferenceEntityLinker
 from src import settings
 
 
+def print_help():
+    print("Usage:\n"
+          "    python3 link_wiki_entities.py <in_file> <out_file> [-p]")
+
+
+def link_entities(paragraph: Paragraph):
+    doc = model(paragraph.text)
+    link_linker.link_entities(paragraph)
+    trained_entity_linker.link_entities(paragraph, doc=doc)
+    alias_entity_linker.link_entities(paragraph, doc=doc)
+    coreference_linker.link_entities(paragraph, doc=doc)
+
+
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print_help()
+        exit(1)
+
     in_file = sys.argv[1]
     out_file = sys.argv[2]
-    corpus = WikipediaCorpus(settings.DATA_DIRECTORY + in_file)
+    is_paragraph_file = "-p" in sys.argv
 
     model = spacy.load(settings.LARGE_MODEL_NAME)
 
@@ -22,13 +40,17 @@ if __name__ == "__main__":
     coreference_linker = CoreferenceEntityLinker(model=model)
 
     with open(settings.DATA_DIRECTORY + out_file, "w") as f:
-        for a_i, article in enumerate(corpus.get_articles()):
-            for paragraph in article.paragraphs:
-                doc = model(paragraph.text)
-                link_linker.link_entities(paragraph)
-                trained_entity_linker.link_entities(paragraph, doc=doc)
-                alias_entity_linker.link_entities(paragraph, doc=doc)
-                coreference_linker.link_entities(paragraph, doc=doc)
-            f.write(article.to_json() + '\n')
-            print("\r%i articles" % (a_i + 1), end='')
+        if is_paragraph_file:
+            for p_i, line in enumerate(open(settings.DATA_DIRECTORY + in_file)):
+                paragraph = paragraph_from_json(line)
+                link_entities(paragraph)
+                f.write(paragraph.to_json() + '\n')
+                print("\r%i paragraphs" % (p_i + 1), end='')
+        else:
+            corpus = WikipediaCorpus(settings.DATA_DIRECTORY + in_file)
+            for a_i, article in enumerate(corpus.get_articles()):
+                for paragraph in article.paragraphs:
+                    link_entities(paragraph)
+                f.write(article.to_json() + '\n')
+                print("\r%i articles" % (a_i + 1), end='')
         print()
