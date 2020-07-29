3834d50a1a1f219a28a0afcc06a1bfddce921eb8
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2020-07-29 13:57:18 +0200

    print progress

--- create_entity_word_vectors_from_extracted_abstracts.py
+++ create_entity_word_vectors_from_extracted_abstracts.py
@@ -1,4 +1,4 @@
-
+import os
 
 from src.word_vectors import VectorGenerator
 from src.entity_database import EntityDatabase
@@ -6,6 +6,8 @@ from src import settings
 
 
 if __name__ == "__main__":
+    SAVE_EVERY = 10000
+
     entity_db = EntityDatabase()
     entity_db.load_entities_big()
     entity_db.load_mapping()
@@ -14,11 +16,21 @@ if __name__ == "__main__":
 
     vector_generator = VectorGenerator()
 
+    vector_dir = settings.DATA_DIRECTORY + "vectors_abstracts/"
+    if not os.path.exists(vector_dir):
+        os.mkdir(vector_dir)
+
     abstracts_file = settings.DATA_DIRECTORY + "article_abstracts.txt"
-    for line in open(abstracts_file):
+    vectors = []
+    file_no = 0
+
+    for i, line in enumerate(open(abstracts_file)):
+        print("\r%i lines read" % i, end="")
         article_id, title, url, abstract = line[:-1].split("\t")
         if article_id in entity_db.wikipedia2wikidata:
             entity_id = entity_db.wikipedia2wikidata[article_id]
             if entity_db.get_entity_frequency(entity_id) > 0:
                 vector = vector_generator.get_vector(abstract)
+                print()
                 print(entity_id, title, vector.shape)
+                #vectors.append()
