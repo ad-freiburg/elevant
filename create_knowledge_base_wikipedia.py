62e1c0053dbadc9d913d647cf6d7c0499c6aecf9
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2020-07-29 15:10:37 +0200

    create directory

--- create_knowledge_base_wikipedia.py
+++ create_knowledge_base_wikipedia.py
@@ -1,3 +1,5 @@
+import os
+
 from src.knowledge_base_creator import KnowledgeBaseCreator
 from src import settings
 
@@ -7,7 +9,14 @@ if __name__ == "__main__":
     print(kb.get_size_entities(), "knowledge base entities")
     print(kb.get_size_aliases(), "knowledge base aliases")
 
-    kb_path = settings.KB_DIRECTORY + "wikipedia/"
+    if not os.path.exists(settings.KB_DIRECTORY):
+        os.mkdir(settings.KB_DIRECTORY)
+
+    save_path = settings.KB_DIRECTORY + "wikipedia/"
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    kb_path = save_path + "kb"
     kb.dump(kb_path)
     print("Saved knowledge base to", kb_path)
     vocab_file = kb_path + "vocab"
