9f1e6a7d1a496c832e9d86c991015e0b5e030343
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2020-07-29 15:04:23 +0200

    read from correct vector directory

--- src/word_vectors.py
+++ src/word_vectors.py
@@ -19,7 +19,7 @@ class VectorLoader:
     @staticmethod
     def iterate(vector_directory: str = settings.VECTORS_DIRECTORY) -> Iterator[Tuple[str, List[int]]]:
         for file in os.listdir(vector_directory):
-            with open(settings.VECTORS_DIRECTORY + file, "rb") as f:
+            with open(vector_directory + file, "rb") as f:
                 vectors = pickle.load(f)
             for entity_id, vector in vectors:
                 yield entity_id, vector
