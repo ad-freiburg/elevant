1a7cc2af42f74e41e6d8e218fdada09f5ae47fd5
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-01-11 12:37:07 +0100

    Enable global entity linking

--- src/linkers/abstract_entity_linker.py
+++ src/linkers/abstract_entity_linker.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple, Optional
 
 from spacy.tokens import Doc
 
+from src.models.entity_mention import EntityMention
 from src.models.entity_prediction import EntityPrediction
 from src.models.wikipedia_article import WikipediaArticle
 
@@ -15,7 +16,8 @@ class AbstractEntityLinker(abc.ABC):
     def predict(self,
                 text: str,
                 doc: Optional[Doc] = None,
-                uppercase: Optional[bool] = False) -> Dict[Tuple[int, int], EntityPrediction]:
+                uppercase: Optional[bool] = False,
+                linked_entities: Optional[Dict[Tuple[int, int], EntityMention]] = None) -> Dict[Tuple[int, int], EntityPrediction]:
         raise NotImplementedError()
 
     @abc.abstractmethod
@@ -26,5 +28,6 @@ class AbstractEntityLinker(abc.ABC):
                       article: WikipediaArticle,
                       doc: Optional[Doc] = None,
                       uppercase: Optional[bool] = False):
-        entity_predictions = self.predict(article.text, doc=doc, uppercase=uppercase)
+        entity_predictions = self.predict(article.text, doc=doc, uppercase=uppercase,
+                                          linked_entities=article.entity_mentions)
         article.link_entities(entity_predictions, self.NER_IDENTIFIER, self.LINKER_IDENTIFIER)
--- src/linkers/trained_entity_linker.py
+++ src/linkers/trained_entity_linker.py
@@ -7,6 +7,7 @@ from spacy.vocab import Vocab
 from spacy.kb import KnowledgeBase, Candidate
 
 from src.linkers.abstract_entity_linker import AbstractEntityLinker
+from src.models.entity_mention import EntityMention
 from src.models.entity_prediction import EntityPrediction
 from src.settings import NER_IGNORE_TAGS
 from src.models.entity_database import EntityDatabase
@@ -52,10 +53,13 @@ class TrainedEntityLinker(AbstractEntityLinker):
         print("loading trained entity linking model...")
         self.loaded_model = torch.load(linker_model_name)
         self.prior = False
+        self.global_model = False
         if type(self.loaded_model) is dict:
             self.linker_model = self.loaded_model['model']
             self.prior = self.loaded_model.get('prior', False)
+            self.global_model = self.loaded_model.get('global_model', False)
             print(f"use prior probs: {self.prior}")
+            print(f"global model: {self.global_model}")
         else:
             self.linker_model = self.loaded_model
         self.linker_model.eval()
@@ -63,7 +67,8 @@ class TrainedEntityLinker(AbstractEntityLinker):
     def predict(self,
                 text: str,
                 doc: Optional[Doc] = None,
-                uppercase: Optional[bool] = False) -> Dict[Tuple[int, int], EntityPrediction]:
+                uppercase: Optional[bool] = False,
+                linked_entities: Optional[Dict[Tuple[int, int], EntityMention]] = None) -> Dict[Tuple[int, int], EntityPrediction]:
         if doc is None:
             doc = self.model(text)
         predictions = {}
@@ -75,7 +80,7 @@ class TrainedEntityLinker(AbstractEntityLinker):
             candidates = self.kb.get_candidates(snippet)
             if not candidates:
                 continue
-            x = self.get_model_input(span, candidates, doc)
+            x = self.get_model_input(span, candidates, doc, linked_entities)
             prediction = self.linker_model(x)
             entity_idx = torch.argmax(prediction).item()
             entity_id = candidates[entity_idx].entity_
@@ -90,7 +95,8 @@ class TrainedEntityLinker(AbstractEntityLinker):
     def get_model_input(self,
                         span: Tuple[int, int],
                         candidates: List[Candidate],
-                        doc: Doc) -> torch.Tensor:
+                        doc: Doc,
+                        linked_entities: Optional[Dict[Tuple[int, int], EntityMention]] = None) -> torch.Tensor:
         """
         Returns the input tensor for the trained model.
         """
@@ -100,7 +106,11 @@ class TrainedEntityLinker(AbstractEntityLinker):
         sentence_vector = EmbeddingsExtractor.get_span_embedding(sentence_span, doc)
 
         # Create empty x tensor
-        n_features = sentence_vector.size(1) * 2
+        if self.global_model:
+            n_features = sentence_vector.size(1) * 3
+            mean_linked_entity_vector = self.get_mean_linked_entity_vector(linked_entities)
+        else:
+            n_features = sentence_vector.size(1) * 2
         if self.prior:
             n_features += 1
         x = torch.empty(size=(len(candidates), n_features))
@@ -111,10 +121,29 @@ class TrainedEntityLinker(AbstractEntityLinker):
             entity_vector = torch.Tensor(self.kb.get_vector(cand.entity_))
             entity_vector = entity_vector.reshape((1, entity_vector.shape[0]))
             input_vector = torch.cat((sentence_vector, entity_vector), dim=1)
+            if self.global_model:
+                input_vector = torch.cat((input_vector, mean_linked_entity_vector), dim=1)
             if self.prior:
                 input_vector = torch.cat((input_vector, torch.Tensor([[cand.prior_prob]])), dim=1)
             x[i] = input_vector
         return x
+
+    def get_mean_linked_entity_vector(self, linked_entities: Optional[Dict[Tuple[int, int], EntityMention]] = None):
+        """
+        Retrieve mean of vectors of entities that were already linked.
+        """
+        if linked_entities is None:
+            # TODO: should later be a random vector as in train_own_linker.py
+            return torch.zeros(1, self.kb.entity_vector_length)
+        else:
+            linked_entity_vectors = torch.zeros(len(linked_entities), self.kb.entity_vector_length)
+        for i, entity_mention in enumerate(sorted(linked_entities.values())):
+            entity_vector = torch.Tensor(self.kb.get_vector(entity_mention.entity_id))
+            entity_vector = entity_vector.reshape((1, entity_vector.shape[0]))
+            linked_entity_vectors[i] = entity_vector
+        mean_linked_entity_vector = torch.mean(linked_entity_vectors, 0)
+        mean_linked_entity_vector = mean_linked_entity_vector.reshape((1, mean_linked_entity_vector.shape[0]))
+        return mean_linked_entity_vector
 
     def has_entity(self, entity_id: str) -> bool:
         return self.kb.contains_entity(entity_id)
