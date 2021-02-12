4498c493d34c3b14c7d2612b6f305af0ad0d2d3b
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-02-12 16:56:06 +0100

    deep copy annotations (fixes that Louis VII, Landgrave of Hesse-Darmstadt is only partially annotated in predictions)

--- evaluation-webapp/show_benchmark.js
+++ evaluation-webapp/show_benchmark.js
@@ -155,9 +155,18 @@ function copy(object) {
     return JSON.parse(JSON.stringify(object));
 }
 
+function deep_copy_array(array) {
+    copied_array = [];
+    for (element of array) {
+        copied_array.push(copy(element));
+    }
+    return copied_array;
+}
+
 function annotate_text(text, annotations, links) {
-    links = links.slice();
-    annotations = annotations.slice();
+    console.log("ANNOTATE");
+    links = deep_copy_array(links);
+    annotations = deep_copy_array(annotations);
     annotations_with_links = [];
     while (annotations.length > 0 || links.length > 0) {
         if (annotations.length == 0) {
@@ -174,6 +183,9 @@ function annotate_text(text, annotations, links) {
             annotation = annotations[0];
             link = links[0];
             link_span = link[0];
+            console.log("step:");
+            console.log(link);
+            console.log(annotation);
             if (link_span[0] < annotation.span[0]) {
                 // add link
                 link_annotation = {
@@ -201,6 +213,7 @@ function annotate_text(text, annotations, links) {
                 annotation["link"] = link[1];
                 annotation_end = Math.min(annotation.span[1], link_span[1]);
                 annotations_with_links.push([[annotation.span[0], annotation_end], annotation]);
+                console.log(annotations_with_links[annotations_with_links.length - 1]);
                 if (annotation_end == link_span[1]) {
                     links.shift();
                 } else {
@@ -214,6 +227,7 @@ function annotate_text(text, annotations, links) {
             }
         }
     }
+    console.log(annotations_with_links);
     for (annotation of annotations_with_links.reverse()) {
         span = annotation[0];
         annotation = annotation[1];
@@ -243,7 +257,7 @@ function annotate_text(text, annotations, links) {
         }
         text = before + replacement + after;
     }
-    annotations.reverse();
+    annotations_with_links.reverse();
     text = text.replaceAll("\n", "<br>");
     return text;
 }
