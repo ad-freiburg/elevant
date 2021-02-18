45942d49e74f3faa5258391859a0ed33f81902fd
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-02-18 12:31:06 +0100

    add bash script to evaluate all approaches

---
+++ evaluate_all.sh
@@ -0,0 +1,11 @@
+python3 evaluate_linked_entities.py results/ambiverse.none.none/ambiverse.none.none.jsonl
+
+python3 evaluate_linked_entities.py results/explosion.ltl.entity/explosion.ltl.entity.jsonl
+
+python3 evaluate_linked_entities.py results/neural_el.ltl.entity/neural_el.ltl.entity.jsonl
+
+python3 evaluate_linked_entities.py results/neural_el.none.none/neural_el.none.none.jsonl
+
+python3 evaluate_linked_entities.py results/tagme/tagme.jsonl
+
+python3 evaluate_linked_entities.py results/wexea.none.none/wexea.none.none.jsonl
