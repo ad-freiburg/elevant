9d821fa6be409f45e4fa7f7e0ea73f8c7d40a0ff
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-02-19 11:28:43 +0100

    do not set error label 'rare' for coreference cases

--- src/evaluation/errors.py
+++ src/evaluation/errors.py
@@ -6,7 +6,8 @@ from src.models.entity_database import EntityDatabase
 
 def label_rare_entity_errors(cases: List[Case], entity_db: EntityDatabase):
     for case in cases:
-        if not case.is_correct() and case.true_entity is not None and case.predicted_entity is not None:
+        if not case.is_correct() and case.true_entity is not None and case.predicted_entity is not None\
+                and not case.is_true_coreference():
             if entity_db.get_sitelink_count(case.true_entity.entity_id) \
                     < entity_db.get_sitelink_count(case.predicted_entity.entity_id):
                 case.add_error_label(ErrorLabel.RARE)
