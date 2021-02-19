19d1a449cb65d33a3e56cf914ea667a0a0100144
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-02-19 17:01:21 +0100

    error label 'partial_name' for false negatives where the mention is a part of the name

--- src/evaluation/errors.py
+++ src/evaluation/errors.py
@@ -7,14 +7,15 @@ from src.models.entity_database import EntityDatabase
 def label_errors(text: str, cases: List[Case], entity_db: EntityDatabase):
     label_demonym_errors(text, cases, entity_db)
     label_rare_entity_errors(text, cases, entity_db)
+    label_partial_name_errors(text, cases, entity_db)
     label_nonentity_coreference_errors(text, cases)
 
 
 def label_demonym_errors(text: str, cases: List[Case], entity_db: EntityDatabase):
     for case in cases:
         if not case.is_correct() and case.true_entity is not None:
-            snippet = text[case.span[0]:case.span[1]]
-            if entity_db.is_demonym(snippet):
+            mention = text[case.span[0]:case.span[1]]
+            if entity_db.is_demonym(mention):
                 case.add_error_label(ErrorLabel.DEMONYM)
 
 
@@ -22,11 +23,21 @@ def label_rare_entity_errors(text: str, cases: List[Case], entity_db: EntityData
     for case in cases:
         if not case.is_correct() and case.true_entity is not None and case.predicted_entity is not None\
                 and not case.is_true_coreference():
-            snippet = text[case.span[0]:case.span[1]]
-            if not entity_db.is_demonym(snippet):
+            mention = text[case.span[0]:case.span[1]]
+            if not entity_db.is_demonym(mention):
                 if entity_db.get_sitelink_count(case.true_entity.entity_id) \
                         < entity_db.get_sitelink_count(case.predicted_entity.entity_id):
                     case.add_error_label(ErrorLabel.RARE)
+
+
+def label_partial_name_errors(text: str, cases: List[Case], entity_db: EntityDatabase):
+    for case in cases:
+        if not case.is_correct() and case.true_entity is not None and not case.is_true_coreference():
+            mention = text[case.span[0]:case.span[1]]
+            if not entity_db.is_demonym(mention):
+                name = case.true_entity.name
+                if len(mention) < len(name) and mention in name.split():
+                    case.add_error_label(ErrorLabel.PARTIAL_NAME)
 
 
 NONENTITY_PRONOUNS = {"it", "this", "that"}
