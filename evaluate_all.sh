5b2bcd35ab94dd9734d2f49092022272157554b0
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-03-04 15:00:50 +0100

    evaluate all jsonl files from evaluation-webapp/evaluation-results/*

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,11 +1,3 @@
-python3 evaluate_linked_entities.py results/ambiverse.none.none/ambiverse.none.none.jsonl
-
-python3 evaluate_linked_entities.py results/explosion.ltl.entity/explosion.ltl.entity.jsonl
-
-python3 evaluate_linked_entities.py results/neural_el.ltl.entity/neural_el.ltl.entity.jsonl
-
-python3 evaluate_linked_entities.py results/neural_el.none.none/neural_el.none.none.jsonl
-
-python3 evaluate_linked_entities.py results/tagme/tagme.jsonl
-
-python3 evaluate_linked_entities.py results/wexea.none.none/wexea.none.none.jsonl
+for d in evaluation-webapp/evaluation-results/*/*.jsonl ; do
+    python3 evaluate_linked_entities.py "$d"
+done
