ba09c318074f36f28c081d2c50ad4fe7d1c4f0bf
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-03-04 15:08:03 +0100

    Add script to create a jsonl file with labels for each benchmark

---
+++ create_benchmark_labels.py
@@ -0,0 +1,30 @@
+import argparse
+
+from src.evaluation.benchmark import Benchmark
+from src.evaluation.examples_generator import get_example_generator
+
+
+def main(args):
+    example_generator = get_example_generator(args.benchmark)
+
+    output_file = open(args.output_file, 'w', encoding='utf8')
+
+    for i, article in enumerate(example_generator.iterate(args.n_articles)):
+        output_file.write(article.to_json() + '\n')
+        print("\r%i articles" % (i + 1), end='')
+    print()
+
+    output_file.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=__doc__)
+
+    parser.add_argument("output_file", type=str, default=None,
+                        help="Output file for the evaluation results")
+    parser.add_argument("-b", "--benchmark", choices=[b.value for b in Benchmark], default=Benchmark.OURS,
+                        help="Benchmark over which to evaluate the linker.")
+    parser.add_argument("-n", "--n_articles", type=int, default=-1,
+                        help="Number of articles to evaluate on.")
+    main(parser.parse_args())
