6b0148f4534f1cc4e17c45269db2c220db1e61e4
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-07-12 15:06:05 +0200

    benchmark analysis + webapp

---
+++ analyze_benchmark_entity_types.py
@@ -0,0 +1,86 @@
+import json
+
+from src.models.wikipedia_article import article_from_json
+from src.settings import WHITELIST_FILE
+
+
+if __name__ == "__main__":
+    in_file = "benchmark/ours_annotated.txt"
+    type_file = "benchmark/qid_to_whitelist_type.tsv"
+
+    # read whitelist
+    whitelist = {}
+    with open(WHITELIST_FILE) as f:
+        for line in f:
+            if line.endswith("\n"):
+                line = line[:-1]
+            qid, name = line.split(":")
+            whitelist[qid] = name
+    print(whitelist)
+
+    # read benchmark
+    articles = []
+    entity_ids = set()
+    with open(in_file) as f:
+        for line in f:
+            article = article_from_json(line)
+            articles.append(article)
+            for label in article.labels:
+                entity_ids.add(label.entity_id)
+
+    # read entity names from whitelist-to-type mapping
+    entity_names = {}
+    with open(type_file, "r", encoding="utf8") as file:
+        for line in file:
+            lst = line.strip().split("\t")
+            entity_id = lst[0][:-1].split("/")[-1]
+            name = lst[1][1:-4]
+            if entity_id in entity_ids:
+                entity_names[entity_id] = name
+
+    labels_tsv_file = in_file.split("_")[0] + ".labels.tsv"
+    types_json_file = in_file.split("_")[0] + ".types.json"
+    labels_tsv_file = open(labels_tsv_file, "w", encoding="utf8")
+
+    print(len(articles))
+    type_counts = {}
+    total_counts = {True: 0, False: 0}
+    num_labels = 0
+    for article in articles:
+        num_labels += len(article.labels)
+        for label in article.labels:
+            mention = article.text[label.span[0]:label.span[1]]
+            if label.entity_id.startswith("Unknown"):
+                types = ["UNKNOWN"]
+            else:
+                types = label.type.split("|")
+            entity_name = entity_names[label.entity_id] if label.entity_id in entity_names else "Unknown"
+            print(mention, label.entity_id, entity_name, types, label.level1)
+            labels_tsv_file.write("\t".join((mention,
+                                             label.entity_id,
+                                             entity_name,
+                                             ", ".join(types),
+                                             str(bool(label.level1))))
+                                  + "\n")
+            for type in types:
+                if type not in type_counts:
+                    type_counts[type] = {True: 0, False : 0}
+                type_counts[type][bool(label.level1)] += 1
+                total_counts[bool(label.level1)] += 1
+
+    labels_tsv_file.close()
+
+    type_name_counts = {}
+    for type in sorted(whitelist) + ["DATETIME", "QUANTITY", "OTHER", "UNKNOWN"]:
+        type_name = whitelist[type] if type in whitelist else type
+        if type in type_counts:
+            lvl1_count = type_counts[type][True]
+            other_count = type_counts[type][False]
+        else:
+            lvl1_count = other_count = 0
+        print(type_name, lvl1_count, other_count)
+        type_name_counts[type_name] = [lvl1_count, other_count]
+
+    with open(types_json_file, "w", encoding="utf8") as f:
+        f.write(json.dumps({"total": [total_counts[True], total_counts[False]],
+                            "types": type_name_counts}))
---
+++ benchmark-webapp/code.js
@@ -0,0 +1,159 @@
+$("document").ready(function() {
+    console.log("document ready");
+    benchmarks = ["ours", "conll"];
+    whitelist = {
+        Q18336849: "item with given name property",
+        Q27096213: "geographic entity",
+        Q483394: "genre",
+        Q43229: "organization",
+        Q17376908: "languoid",
+        Q17537576: "creative work",
+        Q2424752: "product",
+        Q431289: "brand",
+        Q43460564: "chemical entity",
+        Q16521: "taxon",
+        Q9174: "religion",
+        Q7257: "ideology",
+        Q4164871: "position",
+        Q12737077: "occupation",
+        Q216353: "title",
+        Q11862829: "academic discipline",
+        Q21070598: "narrative entity",
+        Q618779: "award",
+        Q431289: "brand",
+        Q12136: "disease",
+        Q4392985: "religious identity",
+        Q11514315: "historical period",
+        Q1656682: "event",
+        Q180684: "conflict",
+        Q373899: "record chart",
+        Q194465: "annexation",
+        Q381072: "crisis",
+        Q22222786: "government program",
+        Q41710: "ethnic group",
+        Q18603729: "dissolution of an administrative territorial entity"
+    }
+    set_table_head();
+    set_table_body();
+});
+
+function set_table_head() {
+    thead = document.getElementById("benchmarks_table_head");
+    head_html = "<tr>";
+    head_html += "<th></th>";
+    for (benchmark of benchmarks) {
+        head_html += "<th>" + benchmark + "</th>";
+    }
+    head_html += "</tr>";
+    thead.innerHTML = head_html;
+}
+
+function set_table_body() {
+    statistics = {};
+    benchmarks.forEach(function(benchmark) {
+        console.log(benchmark);
+        $.get("benchmark/" + benchmark + ".types.json", function(data) {
+            console.log(data);
+            statistics[benchmark] = data;
+            console.log(statistics);
+            console.log(Object.keys(statistics).length);
+            if (Object.keys(statistics).length == benchmarks.length) {
+                fill_table();
+                prepare_selectors();
+            }
+        });
+    });
+}
+
+function fill_table() {
+    body_html = "<tr>";
+    body_html += "<td><b>total</b><br>level 1<br>other</td>";
+    for (benchmark of benchmarks) {
+        level1 = statistics[benchmark]["total"][0];
+        other = statistics[benchmark]["total"][1];
+        sum = level1 + other;
+        body_html += "<td><b>" + sum + "</b><br>";
+        body_html += level1 + " (" + (level1 / sum * 100).toFixed(2) + " %)<br>";
+        body_html += other + " (" + (other / sum * 100).toFixed(2) + "%)</td>";
+    }
+    body_html += "</tr>";
+    for (type of Object.keys(statistics[benchmarks[0]].types)) {
+        console.log(type);
+        body_html += "<tr><td><b>" + type + "</b><br>level 1<br>other" + "</td>";
+        for (benchmark of benchmarks) {
+            total = statistics[benchmark]["total"][0] + statistics[benchmark]["total"][1];
+            level1 = statistics[benchmark].types[type][0];
+            other = statistics[benchmark].types[type][1];
+            sum = level1 + other;
+            sum_percentage = (sum / total * 100).toFixed(2) + "%";
+            level1_percentage = (level1 / total * 100).toFixed(2) + "%";
+            other_percentage = (other / total * 100).toFixed(2) + "%";
+            table_entry = "<b>" + sum + " (" + sum_percentage + ")</b><br>";
+            table_entry += level1 + " (" + level1_percentage + ")<br>";
+            table_entry += other + " (" + other_percentage + ")<br>";
+            body_html += "<td>" + table_entry + "</td>";
+        }
+        body_html += "</tr>";
+    }
+    document.getElementById("benchmarks_table_body").innerHTML = body_html;
+}
+
+function prepare_selectors() {
+    benchmark_select = document.getElementById("benchmark_select");
+    for (benchmark of benchmarks) {
+        var option = document.createElement("option");
+        option.text = benchmark;
+        option.value = benchmark;
+        benchmark_select.add(option);
+    }
+    type_select = document.getElementById("type_select");
+    for (type of Object.keys(statistics[benchmarks[0]].types)) {
+        var option = document.createElement("option");
+        option.text = type;
+        option.value = type;
+        type_select.add(option);
+    }
+    show_entity_table();
+}
+
+function show_entity_table() {
+    benchmark = document.getElementById("benchmark_select").value;
+    type = document.getElementById("type_select").value;
+    tbody = document.getElementById("entities_table_body");
+    tbody.innerHTML = "";
+    html_text = "";
+    $.get("benchmark/" + benchmark + ".labels.tsv", function(data) {
+        lines = data.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            line = lines[i];
+            if (line.length > 0) {
+                vals = line.split("\t");
+                types = vals[3].split(", ");
+                is_selected = false;
+                type_names = "";
+                for (tp of types) {
+                    if (whitelist.hasOwnProperty(tp)) {
+                        tp = whitelist[tp];
+                    }
+                    if (type == tp) {
+                        is_selected = true;
+                    }
+                    if (type_names.length > 0) {
+                        type_names += ", ";
+                    }
+                    type_names += tp;
+                }
+                vals[3] = type_names;
+                if (is_selected) {
+                    row = "<tr>";
+                    for (val of vals) {
+                        row += "<td>" + val + "</td>";
+                    }
+                    row += "</tr>";
+                    html_text += row;
+                }
+            }
+        }
+        tbody.innerHTML = html_text;
+    });
+}
