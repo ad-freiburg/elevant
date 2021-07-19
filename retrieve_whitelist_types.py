6dbe1b26b344843a038ef474d50f61773df8c40f
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-07-19 13:25:53 +0200

    Add query for repairing the occupation data error

--- retrieve_whitelist_types.py
+++ retrieve_whitelist_types.py
@@ -3,6 +3,37 @@ import argparse
 from src import settings
 
 
+QUERY = "PREFIX wdt: <http://www.wikidata.org/prop/direct/> " \
+        "PREFIX wd: <http://www.wikidata.org/entity/> " \
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" \
+        "SELECT DISTINCT ?item ?label ?type WHERE " \
+        "{ { ?item wdt:P31 ?type . " \
+        "?item @en@rdfs:label ?label ." \
+        "VALUES ?type { %s } } " \
+        "UNION " \
+        "{ ?item wdt:P31 ?m . ?m wdt:P279+ ?type . " \
+        "?item @en@rdfs:label ?label ." \
+        "VALUES ?type { %s } } }"
+
+QUERY_REPAIR_DATA_ERROR = "PREFIX wdt: <http://www.wikidata.org/prop/direct/> " \
+        "PREFIX wd: <http://www.wikidata.org/entity/> " \
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" \
+        "SELECT DISTINCT ?item ?label ?type WHERE " \
+        "{ { { ?item wdt:P31 ?type . " \
+        "?item @en@rdfs:label ?label . " \
+        "VALUES ?type { %s } } " \
+        "UNION " \
+        "{ ?item wdt:P31 ?m . ?m wdt:P279+ ?m2 . ?m2 wdt:P279 ?type . " \
+        "?item @en@rdfs:label ?label . " \
+        "VALUES ?type { %s } " \
+        "MINUS { VALUES (?m2 ?type) {(wd:Q24229398 wd:Q12737077)} } " \
+        "} } UNION { " \
+        "?item wdt:P31 ?m . ?m wdt:P279 ?type . " \
+        "?item @en@rdfs:label ?label . " \
+        "VALUES ?type { %s }" \
+        "MINUS { VALUES (?m ?type) {(wd:Q24229398 wd:Q12737077)} }" \
+        "} }"
+
 def get_whitelist(whitelist_file):
     whitelist_str = ""
     with open(whitelist_file, "r", encoding="utf8") as file:
@@ -15,17 +46,8 @@ def get_whitelist(whitelist_file):
 
 def main(args):
     whitelist = get_whitelist(args.whitelist_file)
-    query = "PREFIX wdt: <http://www.wikidata.org/prop/direct/> " \
-        "PREFIX wd: <http://www.wikidata.org/entity/> " \
-        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" \
-        "SELECT DISTINCT ?item ?label ?type WHERE " \
-        "{ { ?item wdt:P31 ?type . " \
-        "?item @en@rdfs:label ?label ." \
-        "VALUES ?type { " + whitelist + " } } " \
-        "UNION " \
-        "{ ?item wdt:P31 ?m . ?m wdt:P279+ ?type . " \
-        "?item @en@rdfs:label ?label ." \
-        "VALUES ?type { " + whitelist + " } } }"
+    # query = QUERY % (whitelist, whitelist)
+    query = QUERY_REPAIR_DATA_ERROR % (whitelist, whitelist, whitelist)
     url = 'http://galera.informatik.privat:7001/'  # Using the proxy can cause Timeout/MaxRetryError
     data = {"query": query, "action": "tsv_export"}
     r = requests.get(url, params=data)
