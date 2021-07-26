b85a7fb36bb40a8d8b732272971b9fc4ae100cd6
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-07-26 16:56:46 +0200

    Specify benchmarks in evaluation script

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,3 +1,15 @@
-for d in evaluation-webapp/evaluation-results/*/*.jsonl ; do
-    python3 evaluate_linked_entities.py "$d"
+for d in whitelist_benchmark_results/*/*.jsonl ; do
+    echo "$d"
+    if [ "${d: -9}" != "ace.jsonl" ] && [ "${d: -11}" != "msnbc.jsonl" ] && [ "${d: -16}" != "conll-test.jsonl" ] && [ "${d: -15}" != "conll-dev.jsonl" ] && [ "${d: -11}" != "conll.jsonl" ]; then
+       # This is not an else branch to make it easier to only execute this branch.
+       python3 evaluate_linked_entities.py "$d" -b ours
+    elif [ "${d: -9}" == "ace.jsonl" ]; then
+        python3 evaluate_linked_entities.py "$d" -b ace
+    elif [ "${d: -11}" == "msnbc.jsonl" ]; then
+        python3 evaluate_linked_entities.py "$d" -b msnbc
+    elif [ "${d: -16}" == "conll-test.jsonl" ]; then
+        python3 evaluate_linked_entities.py "$d" -b conll-test
+    elif [ "${d: -15}" == "conll-dev.jsonl" ]; then
+        python3 evaluate_linked_entities.py "$d" -b conll-dev
+    fi
 done
