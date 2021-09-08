d4ce9fb89efc2cbc9de3ebd4fc3713737bd8d310
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-09-08 15:39:09 +0200

    Add newscrawl benchmark

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,8 +1,10 @@
-for d in whitelist_benchmark_results/*/*.jsonl ; do
+for d in evaluation_results/*/*.jsonl ; do
     echo "$d"
     if [ "${d: -9}" != "ace.jsonl" ] && [ "${d: -11}" != "msnbc.jsonl" ] && [ "${d: -16}" != "conll-test.jsonl" ] && [ "${d: -15}" != "conll-dev.jsonl" ] && [ "${d: -11}" != "conll.jsonl" ]; then
        # This is not an else branch to make it easier to only execute this branch.
        python3 evaluate_linked_entities.py "$d" -b ours
+    elif [ "${d: -15}" == "newscrawl.jsonl" ]; then
+        python3 evaluate_linked_entities.py "$d" -b newscrawl
     elif [ "${d: -9}" == "ace.jsonl" ]; then
         python3 evaluate_linked_entities.py "$d" -b ace --no-unknowns
     elif [ "${d: -11}" == "msnbc.jsonl" ]; then
