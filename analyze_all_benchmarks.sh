579ea51e497c9d887fcf02d4cab05fc1ce0f7609
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-09-09 10:51:22 +0200

    Add newscrawl benchmark to entity type analysis

--- analyze_all_benchmarks.sh
+++ analyze_all_benchmarks.sh
@@ -1,3 +1,3 @@
-for benchmark in ours conll conll-dev conll-test msnbc ace; do
+for benchmark in ours conll conll-dev conll-test msnbc ace newscrawl; do
 	python3 analyze_benchmark_entity_types.py $benchmark;
 done
