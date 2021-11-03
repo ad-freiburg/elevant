c91fe220e87a4b05ef0dbc387029b8d2a11b4993
Natalie Prange <prange@informatik.uni-freiburg.de>
2021-11-03 09:36:44 +0100

    Add mine, yours, hers, ours, theirs to pronouns

--- src/utils/pronoun_finder.py
+++ src/utils/pronoun_finder.py
@@ -6,14 +6,16 @@ from src.models.gender import Gender
 
 class PronounFinder:
     pronoun_genders = {"i": Gender.UNKNOWN, "my": Gender.UNKNOWN, "me": Gender.UNKNOWN, "myself": Gender.UNKNOWN,
+                       "mine": Gender.UNKNOWN,
                        "you": Gender.UNKNOWN, "your": Gender.UNKNOWN, "yourself": Gender.UNKNOWN,
+                       "yours": Gender.UNKNOWN,
                        "he": Gender.MALE, "his": Gender.MALE, "him": Gender.MALE, "himself": Gender.MALE,
-                       "she": Gender.FEMALE, "her": Gender.FEMALE, "herself": Gender.FEMALE,
+                       "she": Gender.FEMALE, "her": Gender.FEMALE, "herself": Gender.FEMALE, "hers": Gender.FEMALE,
                        "it": Gender.NEUTRAL, "its": Gender.NEUTRAL, "itself": Gender.NEUTRAL,
-                       "us": Gender.UNKNOWN, "our": Gender.UNKNOWN, "ourselves": Gender.UNKNOWN,
+                       "us": Gender.UNKNOWN, "our": Gender.UNKNOWN, "ourselves": Gender.UNKNOWN, "ours": Gender.UNKNOWN,
                        "yourselves": Gender.UNKNOWN,
                        "they": Gender.UNKNOWN, "their": Gender.UNKNOWN, "them": Gender.UNKNOWN,
-                       "themselves": Gender.UNKNOWN}
+                       "themselves": Gender.UNKNOWN, "theirs": Gender.UNKNOWN}
 
     @classmethod
     def is_pronoun(cls, text: str) -> bool:
