a18277bf423d06752242cbc0712540bbee74c5fc
Natalie Prange <prange@informatik.uni-freiburg.de>
2022-07-21 14:27:31 +0200

    For spacy, make wikipedia model the default instead of wikidata

--- src/linkers/spacy_linker.py
+++ src/linkers/spacy_linker.py
@@ -17,8 +17,8 @@ class SpacyLinker(AbstractEntityLinker):
         # Get config variables
         self.linker_identifier = config["name"] if "name" in config else "Spacy"
         self.ner_identifier = "EnhancedSpacy"
-        model_name = config["model_name"] if "model_name" in config else "prior_trained"
-        kb_name = config["kb"] if "kb" in config else "wikidata"
+        model_name = config["model_name"] if "model_name" in config else "wikipedia"
+        kb_name = config["kb"] if "kb" in config else "wikipedia"
 
         self.model = EntityLinkerLoader.load_trained_linker(model_name, kb_name=kb_name)
         if not self.model.has_pipe("ner_postprocessor"):
