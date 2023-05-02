041040e78f117a19bbe6f905c11a2cf8382bf4c8
Natalie Prange <prange@informatik.uni-freiburg.de>
2023-05-02 14:45:42 +0200

    Make performance improvements, e.g. by using binary search
    This reduces the runtime of our coreference linker by around 80% O.o

--- src/utils/offset_converter.py
+++ src/utils/offset_converter.py
@@ -1,54 +1,79 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from spacy.tokens import Doc, Token, Span
 
 
 class OffsetConverter:
     @staticmethod
+    def get_token_idx(offset: int, doc: Doc, left: Optional[int] = None, right: Optional[int] = None) -> int:
+        """
+        Performs binary search on the list of token indices in the doc to find
+        the token that contains the character at position offset. If the offset
+        is not found, returns None.
+        """
+        if left is None:
+            left = 0
+        if right is None:
+            right = len(doc) - 1
+        while left <= right:
+            mid = (right + left) // 2
+            # If the index (thus the -1 !) of the last character of the current token is
+            # smaller than the offset we're looking for, search in right part.
+            if doc[mid].idx + len(doc[mid].text) - 1 < offset:
+                left = mid + 1
+            # If the index of the first character of the current token is
+            # greater than the offset we're looking for, search in left part.
+            elif doc[mid].idx > offset:
+                right = mid - 1
+            # Offset is present at mid
+            else:
+                return mid
+
+    @staticmethod
     def get_token(offset: int, doc: Doc) -> Token:
-        for token in doc:
-            if offset < token.idx + len(token.text):
-                return token
+        """
+        Get the token that contains the character at position offset.
+        """
+        token_index = OffsetConverter.get_token_idx(offset, doc)
+        if token_index is not None:
+            return doc[token_index]
+
+    @staticmethod
+    def get_tokens_in_span(span: Tuple[int, int], doc: Doc) -> List[Token]:
+        """
+        Get all tokens that are fully contained in the given span.
+        """
+        # Get the index of the token that contains the start of the span
+        left_i = OffsetConverter.get_token_idx(span[0], doc)
+        # We only want fully contained tokens, i.e. no tokens that start before the span starts
+        left_i = left_i if doc[left_i].idx >= span[0] else left_i + 1
+        # Get the index of the token that contains the end of the span
+        right_i = OffsetConverter.get_token_idx(span[1] - 1, doc)  # -1 because right end of span is exclusive
+        # We only want fully contained tokens, i.e. no tokens that end after the span ends
+        right_i = right_i if doc[right_i].idx + len(doc[right_i].text) <= span[1] else right_i - 1
+        # Get all contained tokens
+        tokens = doc[left_i:right_i + 1]  # + 1 because the token at right_i is fully contained in the span
+        return tokens
 
     @staticmethod
     def get_sentence(offset: int, doc: Doc) -> Span:
+        """
+        Get the span of the sentence that contains the character at position
+        offset. Binary search is not possible here, since doc.sents is an
+        iterator, so getting the length of doc.sents is in O(n).
+        """
         for i, sent in enumerate(doc.sents):
             if sent.end_char >= offset:
                 return sent
 
     @staticmethod
-    def get_sentence_idx(offset: int, doc: Doc) -> int:
-        for i, sent in enumerate(doc.sents):
-            if sent.end_char >= offset:
-                return i
-
-    @staticmethod
-    def get_containing_sents(offset: int, doc: Doc, sents_before=4) -> List[Span]:
-        containing_sents = []
-        for i, sent in enumerate(doc.sents):
-            if len(containing_sents) > sents_before:
-                containing_sents.pop(0)
-            containing_sents.append(sent)
-            if sent.end_char > offset:
-                return containing_sents
-
-    @staticmethod
-    def get_tokens_in_span(span: Tuple[int, int], doc: Doc) -> List[Token]:
-        tokens = []
-        for token in doc:
-            if token.idx >= span[0] and token.idx + len(token.text) <= span[1]:
-                tokens.append(token)
-        return tokens
-
-    @staticmethod
-    def get_token_idx(offset: int, doc: Doc) -> Token:
-        for i, token in enumerate(doc):
-            if offset < token.idx + len(token.text):
-                return i
-
-    @staticmethod
     def get_token_idx_in_sent(offset: int, doc: Doc) -> int:
-        for sent in doc.sents:
-            for i, tok in enumerate(sent):
-                if tok.idx >= offset:
-                    return i
+        """
+        For the token that contains the character at position offset, get the
+        token index within the sentence that contains it. I.e. if it's the
+        first token in the sentence return 0.
+        """
+        token = OffsetConverter.get_token(offset, doc)
+        for i, tok in enumerate(token.sent):
+            if tok.idx >= offset:
+                return i
