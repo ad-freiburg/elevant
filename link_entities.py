b6fa1c3a7e2d151a22b512766a5d572ecc7ecb3a
Natalie Prange <prange@informatik.uni-freiburg.de>
2023-07-27 13:08:44 +0200

    Fix multiprocessing double RAM usage

--- link_entities.py
+++ link_entities.py
@@ -1,9 +1,9 @@
 """
-Links entities in a given file to their WikiData entry.
-The linker pipeline consists of two main components:
+Links entities in a given file to their Wikidata entry.
+The linker pipeline consists of three main components:
 
-    1) linker: use a NER system and a NED system to detect and link remaining
-       entity mentions.
+    1) linker: use a NER system and a NED system to detect and link entity
+       mentions to their Wikidata entry.
     2) coreference linker: link coreferences to their Wikidata entry.
 
 For each component, you can choose between different linker variants or omit
@@ -11,15 +11,7 @@ the component from the pipeline.
 The result is written to a given output file in jsonl format with one article
 per line.
 
-When using multiprocessing, due to the forkserver, the linking system will be
-loaded twice, once on import and once when calling set_forkserver_preload().
-See also
-https://stackoverflow.com/questions/63424251/multiprocessing-in-python-what-gets-inherited-by-forkserver-process-from-paren
-
-However, except for reading in articles, memory usage should not fluctuate
-much and forkserver is apparently more stable than fork so we don't have
-to manually collect stuck processes after a certain amount of time.
-About 90GB of RAM are required when using the full linker pipeline.
+About 55GB of RAM are required when using the full linker pipeline.
 """
 
 import argparse
@@ -28,74 +20,25 @@ import os
 import sys
 import time
 import log
+import multiprocessing
 
 from src import settings
 from src.linkers.linkers import Linkers, CoreferenceLinkers
 from src.models.article import Article, article_from_json
 from src.helpers.wikipedia_dump_reader import WikipediaDumpReader
 
-# This has to happen here, so I can write the configuration file for the linking_system
-# before importing it
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
-
-parser.add_argument("input_file", type=str,
-                    help="Input file with articles in JSON format or raw text.")
-parser.add_argument("output_file", type=str,
-                    help="Output file.")
-parser.add_argument("-l", "--linker_name", choices=[li.value for li in Linkers], required=True,
-                    help="Entity linker name.")
-parser.add_argument("--linker_config",
-                    help="Configuration file for the specified linker."
-                         "Per default, the system looks for the config file at configs/<linker_name>.config.json")
-parser.add_argument("-raw", "--raw_input", action="store_true",
-                    help="Set to use an input file with raw text.")
-parser.add_argument("--article_format", action="store_true",
-                    help="The input file is in our article jsonl format.")
-parser.add_argument("-n", "--n_articles", type=int, default=-1,
-                    help="Number of articles to link.")
-parser.add_argument("-coref", "--coreference_linker", choices=[cl.value for cl in CoreferenceLinkers],
-                    help="Coreference linker to apply after entity linkers.")
-parser.add_argument("--only_pronouns", action="store_true",
-                    help="Only link coreferences that are pronouns.")
-parser.add_argument("-min", "--minimum_score", type=int, default=0,
-                    help="Minimum entity score to include entity in database")
-parser.add_argument("-small", "--small_database", action="store_true",
-                    help="Load a small version of the database")
-parser.add_argument("--uppercase", action="store_true",
-                    help="Set to remove all predictions on snippets which do not contain an uppercase character.")
-parser.add_argument("--type_mapping", type=str, default=settings.QID_TO_WHITELIST_TYPES_DB,
-                    help="For pure prior linker: Map predicted entities to types using the given mapping.")
-parser.add_argument("-m", "--multiprocessing", type=int, default=1,
-                    help="Number of processes to use. Default is 1, i.e. no multiprocessing. "
-                         "Note that RAM usage doubles when using multiprocessing.")
-
-args = parser.parse_args()
-
-# Don't write the log output to a file as the logger does usually.
-# Otherwise a FileNotFoundError is thrown.
-logger = log.setup_logger(sys.argv[0], write_to_file=False)
-logger.debug(' '.join(sys.argv))
-
-# Write command line arguments to temporary config file which is then read by the forkserver_linking_system module.
-# This is not pretty, but I don't see a better solution where the user can still use command line arguments to
-# configure the linking_system.
-config = {"linker_name": args.linker_name,
-          "linker_config": args.linker_config,
-          "link_linker": args.link_linker,
-          "coreference_linker": args.coreference_linker,
-          "minimum_score": args.minimum_score,
-          "type_mapping": args.type_mapping}
-with open(settings.TMP_FORKSERVER_CONFIG_FILE, "w", encoding="utf8") as config_file:
-    json.dump(config, config_file)
-
-# This has to happen here, otherwise a linking_system is not defined error is thrown.
-from src.linkers.forkserver_linking_system import linking_system
-
-import multiprocessing
-
 # Don't show dependencygraph UserWarning: "The graph doesn't contain a node that depends on the root element."
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
+
+# Importing the linking_system has to happen here, otherwise a linking_system is not defined error is thrown.
+# In order to not load several GB of mappings twice, only do a fake loading such that the name is imported,
+# but no mappings are loaded
+config = {"no_loading": True}
+with open(settings.TMP_FORKSERVER_CONFIG_FILE, "w", encoding="utf8") as config_file:
+    json.dump(config, config_file)
+from src.linkers.forkserver_linking_system import linking_system
+
 
 CHUNK_SIZE = 10  # Number of articles submitted to one process as a single task
 MAX_TASKS_PER_CHILD = 5
@@ -133,15 +76,16 @@ def main():
 
     output_file = open(args.output_file, 'w', encoding='utf8')
 
-    logger.info("Start linking using %d processes." % args.multiprocessing)
     i = 0
     iterator = article_iterator(args.input_file)
     if args.multiprocessing > 1:
+        logger.info("Loading linking system...")
         multiprocessing.set_start_method('forkserver')
         multiprocessing.set_forkserver_preload(["src.linkers.forkserver_linking_system"])
         start = time.time()
         last_time = start
         with multiprocessing.Pool(processes=args.multiprocessing, maxtasksperchild=MAX_TASKS_PER_CHILD) as executor:
+            logger.info("Start linking using %d processes." % args.multiprocessing)
             for article in executor.imap(link_entities_tuple_argument, iterator, chunksize=CHUNK_SIZE):
                 output_file.write(f"{article.to_json(evaluation_format=False)}\n")
                 i += 1
@@ -155,10 +99,17 @@ def main():
                     last_time = time.time()
         i -= 1  # So final log reports correct number of linked articles with and without multiprocessing
     else:
+        from src.linkers.linking_system import LinkingSystem
+        ls = LinkingSystem(args.linker_name,
+                           args.linker_config,
+                           coref_linker=args.coreference_linker,
+                           min_score=args.minimum_score,
+                           type_mapping_file=args.type_mapping)
+        logger.info("Start linking with a single process.")
         start = time.time()
         for i, tupl in enumerate(iterator):
             article, uppercase, only_pronouns = tupl
-            linking_system.link_entities(article, uppercase, only_pronouns)
+            ls.link_entities(article, uppercase, only_pronouns)
             output_file.write(f"{article.to_json(evaluation_format=False)}\n")
             total_time = time.time() - start
             time_per_article = total_time / (i + 1)
@@ -171,4 +122,55 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
+
+    parser.add_argument("input_file", type=str,
+                        help="Input file with articles in JSON format or raw text.")
+    parser.add_argument("output_file", type=str,
+                        help="Output file.")
+    parser.add_argument("-l", "--linker_name", choices=[li.value for li in Linkers], required=True,
+                        help="Entity linker name.")
+    parser.add_argument("--linker_config",
+                        help="Configuration file for the specified linker."
+                             "Per default, the system looks for the config file at configs/<linker_name>.config.json")
+    parser.add_argument("-raw", "--raw_input", action="store_true",
+                        help="Set to use an input file with raw text.")
+    parser.add_argument("--article_format", action="store_true",
+                        help="The input file is in our article jsonl format.")
+    parser.add_argument("-n", "--n_articles", type=int, default=-1,
+                        help="Number of articles to link.")
+    parser.add_argument("-coref", "--coreference_linker", choices=[cl.value for cl in CoreferenceLinkers],
+                        help="Coreference linker to apply after entity linkers.")
+    parser.add_argument("--only_pronouns", action="store_true",
+                        help="Only link coreferences that are pronouns.")
+    parser.add_argument("-min", "--minimum_score", type=int, default=0,
+                        help="Minimum entity score to include entity in database")
+    parser.add_argument("-small", "--small_database", action="store_true",
+                        help="Load a small version of the database")
+    parser.add_argument("--uppercase", action="store_true",
+                        help="Set to remove all predictions on snippets which do not contain an uppercase character.")
+    parser.add_argument("--type_mapping", type=str, default=settings.QID_TO_WHITELIST_TYPES_DB,
+                        help="For pure prior linker: Map predicted entities to types using the given mapping.")
+    parser.add_argument("-m", "--multiprocessing", type=int, default=1,
+                        help="Number of processes to use. Default is 1, i.e. no multiprocessing. "
+                             "Note that RAM usage doubles when using multiprocessing.")
+
+    args = parser.parse_args()
+
+    # Don't write the log output to a file as the logger does usually.
+    # Otherwise a FileNotFoundError is thrown.
+    logger = log.setup_logger(sys.argv[0], write_to_file=False)
+    logger.debug(' '.join(sys.argv))
+
+    # Write command line arguments to temporary config file which is then read by the forkserver_linking_system module.
+    # This is not mega pretty, but I don't see a better solution where the user can still use command line arguments to
+    # configure the linking_system.
+    config = {"linker_name": args.linker_name,
+              "linker_config": args.linker_config,
+              "coreference_linker": args.coreference_linker,
+              "minimum_score": args.minimum_score,
+              "type_mapping": args.type_mapping}
+    with open(settings.TMP_FORKSERVER_CONFIG_FILE, "w", encoding="utf8") as config_file:
+        json.dump(config, config_file)
+
     main()
--- src/linkers/forkserver_linking_system.py
+++ src/linkers/forkserver_linking_system.py
@@ -5,8 +5,11 @@ import json
 with open(settings.TMP_FORKSERVER_CONFIG_FILE, "r", encoding="utf8") as file:
     config = json.load(file)
 
-linking_system = LinkingSystem(config["linker_name"],
-                               config["linker_config"],
-                               coref_linker=config["coreference_linker"],
-                               min_score=config["minimum_score"],
-                               type_mapping_file=config["type_mapping"])
+if "no_loading" in config and config["no_loading"]:
+    linking_system = None
+else:
+    linking_system = LinkingSystem(config["linker_name"],
+                                   config["linker_config"],
+                                   coref_linker=config["coreference_linker"],
+                                   min_score=config["minimum_score"],
+                                   type_mapping_file=config["type_mapping"])
