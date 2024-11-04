3348ca6e72721e1239c1f7fe2322678b76964c0f
Natalie Prange <prange@informatik.uni-freiburg.de>
2024-11-04 12:28:27 +0100

    Use native F-Coref instead of spacy component for performance
    The spacy component is for some reason extremely slow (130s vs. 6s on
    Wiki-Fair v2.0 test set using GPU for both runs).

--- src/elevant/linkers/fastcoref_coref_linker.py
+++ src/elevant/linkers/fastcoref_coref_linker.py
@@ -1,31 +1,25 @@
 from typing import Optional, List
 
-import spacy
 from spacy.tokens import Doc
 from spacy.language import Language
-from elevant import settings
 from elevant.linkers.abstract_coref_linker import AbstractCorefLinker
 from elevant.models.coref_cluster import CorefCluster
 from elevant.models.article import Article
-from fastcoref import spacy_component
+from fastcoref import FCoref
 
 
 class FastcorefCorefLinker(AbstractCorefLinker):
     def __init__(self,
                  model: Optional[Language] = None):
-        if model is None:
-            self.model = spacy.load(settings.LARGE_MODEL_NAME, disable=["lemmatizer"])
-        else:
-            self.model = model
-        self.model.add_pipe("fastcoref")
-        # The fastcoref module could also be further configured by replacing the line above with something like this:
-        # self.model.add_pipe("fastcoref", config={'model_architecture': 'LingMessCoref', 'model_path': 'biu-nlp/lingmess-coref', 'device': 'cpu'})
+        # Use native F-Coref instead of the spacy component, as the spacy component is significantly slower
+        # (on Wiki-Fair v2.0 test set: 130s vs. 6s on GPU)
+        self.model = FCoref(device='cuda:0')
 
     def get_clusters(self, article: Article, doc: Optional[Doc] = None) -> List[CorefCluster]:
-        doc = self.model(article.text)
+        preds = self.model.predict(texts=[article.text])
 
         coref_clusters = []
-        for cluster in doc._.coref_clusters:
+        for cluster in preds[0].get_clusters(as_strings=False):
             main = cluster[0]  # Assume the first mention in the cluster is the main mention
             coref_cluster = CorefCluster(main, cluster)
             coref_clusters.append(coref_cluster)
