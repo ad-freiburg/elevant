e1cdf2ac271027a5a0374874193a19c612c0a773
mb504 <>
2026-04-01 14:50:39 +0200

    forgot to add the latest grasp_linker.py in the last commit

--- src/elevant/linkers/grasp_linker.py
+++ src/elevant/linkers/grasp_linker.py
@@ -14,14 +14,16 @@ class GraspLinker(AbstractEntityLinker):
         self.model = None
         self.entity_db = entity_db
         self.url = config["url"] # url of the grasp server
-        self.log_directory = config.get("output_directory", "")
-        self.benchmark = config.get("benchmark", "benchmark")
-        self.overlap = config.get("overlap_between_windows", 32)
-        self.window_size = config.get("window_size", 500)
-        self.num_retries = config.get("num_retries", 3)
-        self.num_parallel = config.get("num_parallel", 1)
-        self.seconds_between_retries = config.get("seconds_between_retries", 20)
-        self.resend_if_timeout = config.get("resend_request_if_timeout_in_output", False)
+        self.log_directory = config.get("log_output_directory", "")
+        self.benchmark = config.get("benchmark_name_for_logging", "benchmark")
+        self.overlap = config.get("overlap_between_windows", 48)
+        self.window_size = config.get("window_size", 400)
+        self.num_retries = config.get("num_retries", 5)
+        self.num_parallel = config.get("num_parallel", 3)
+        self.split_at_punctuations = config.get("split_at_punctuations", False)
+        self.num_allowed_timeouts = config.get("num_allowed_timeouts", 0)
+        self.seconds_between_retries = config.get("seconds_between_retries", 30)
+        self.resend_if_timeout = config.get("resend_if_timeout_in_output", True)
         self.log_path = os.path.join(self.log_directory, f"{self.benchmark}.jsonl")
         self.text_counter = 0
         try:
@@ -32,16 +34,27 @@ class GraspLinker(AbstractEntityLinker):
     
 
     def get_text_split_indices(self, text: str) -> list[tuple[int, int]]:
-        # alternative stupid version: result = [(i, i + self.window_size + self.overlap) for i in range(0, len(text), self.window_size)]
         result = []
         last_split = 0
-        split_sequences = {". ", ", ", ": ", "; ", "\n"}
+        split_sequences = {". ", ": ", "; ", "! ", "? "}
         while last_split + self.window_size < len(text):
-            for i in range(self.overlap):
-                if text[last_split + self.window_size + i-1: i] in split_sequences:
-                    result += [(last_split, i)]
-                    last_split = i
+            # start index for the next interval
+            next_last_split = last_split + self.window_size - self.overlap
+
+            # default for current end index if we find no split sequence in the overlap
+            current_split = last_split + self.window_size
+
+            # looking for a split sequence to potentially have less overlap
+            if self.split_at_punctuations:
+                for i in range(self.overlap - 1):
+                    if text[next_last_split + i
+                            : next_last_split + i + 2] in split_sequences:
+                        current_split = next_last_split + i + 2
+                        next_last_split = next_last_split + i + 2
+                        break
             
+            result += [(last_split, current_split)]
+            last_split = next_last_split
 
         result += [(last_split, len(text))]
         return result
@@ -59,25 +72,33 @@ class GraspLinker(AbstractEntityLinker):
 
     def send_a_request(self, text: dict) -> dict:
         rq = {"task": "entity-linking", "input": text, "knowledge_graphs": ["wikidata"]}
-        for retry in range(self.num_retries):
+        for retry in range(self.num_retries + 1):
             try:
-                response = requests.post(self.url, json=rq).json()
+                response = requests.post(self.url, json=rq)
+                response = response.json()
             except:
                 print(f"Error sending request for {text['start']} to {text['end']}, "
-                      f"{self.num_retries - retry - 1} retries left. ")
+                      f"{self.num_retries - retry} retries left. ")
                 time.sleep(self.seconds_between_retries)
                 continue
 
             with open(self.log_path, "a") as logfile:
                 json.dump(response, logfile)
 
-            # look for the word 'timeout' in the log
+            # look for words 'SPARQL query timed out' in the results
             predictions = response.get("output", {}).get("predictions", [])
-            if (self.resend_if_timeout and 
-                "timeout" in response.get("output", {}).get("messages", "")
-            ):
-                print(f"resending excerpt from {text['start']} to {text['end']}")
-                continue
+            if self.resend_if_timeout and retry < self.num_retries:
+                timeout_counter = 0
+                messages = response.get("messages", [])
+                for message in messages:
+                    timeout_counter += str(message).count("SPARQL query timed out")
+                if timeout_counter > self.num_allowed_timeouts:
+                    print(
+                        f"Too many timeouts ({timeout_counter}), resending excerpt " 
+                        f"from {text['start']} to {text['end']}, "
+                        f"{self.num_retries - retry} retries left. "
+                    )
+                    continue
 
             print(f"Annotated excerpt from {text['start']} to {text['end']}")
             return self.dict_to_preds(predictions)
@@ -93,7 +114,7 @@ class GraspLinker(AbstractEntityLinker):
         self.text_counter += 1
 
         headline = text[0:48].split("\n")[0]
-        print(f"\nText {self.text_counter} of lenght {len(text)}: '{headline}'.")
+        print(f"\nText {self.text_counter} of lenght {len(text)}: '{headline}'")
 
         # split the text into smaller pieces and tell grasp which excerpts to annotate
         splits = self.get_text_split_indices(text)
